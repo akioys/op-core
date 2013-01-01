71d97ac4948c8938c17fbe12dc120c77ff9d8293
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2013-01-01 14:49:20 +0900

    [PDO/DML5] edit: Anti-notice only

--- PDO/DML5.class.php
+++ PDO/DML5.class.php
@@ -480,6 +480,7 @@ class DML5 extends OnePiece5
 		//  init
 		$join = array();
 		$agg  = array();
+		$return = null;
 		
 		if( isset($conf['column']) ){
 			if( is_array($conf['column']) ){
@@ -517,9 +518,7 @@ class DML5 extends OnePiece5
 		//  exists select column
 		$count = count($join);
 		if( $count ){
-				$this->mark($count);
 			if( $count === 1 ){
-				$this->mark($count);
 				if( !$join[0] ){
 					return '*';
 				}
