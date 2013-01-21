43faf71e558fcc81f24b883b338db96c89d55d4a
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2013-01-21 14:39:14 +0900

    [OnePiece5] bug-fix: object is var_export

--- OnePiece5.class.php
+++ OnePiece5.class.php
@@ -979,7 +979,12 @@ __EOL__;
 				if(!$args[0]){
 					$args = '![.red[null]]';
 				}else{
-					$str = var_export($args[0],true);
+					if( is_object($args[0]) ){
+						$var_export = get_class($args[0]);
+					}else{
+						$var_export = var_export($args[0],true);
+					}
+					$str = $var_export;
 					$str = str_replace(array("\n","\r","\t"), '', $str);
 					$str = str_replace("\\'", "'", $str);
 					$str = str_replace(",)", ") ", $str);
