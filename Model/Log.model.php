a63a82625544f5dc7ac159c0a54aed95ed854243
TomoakiNagahara <tomoaki.nagahara@gmail.com>
2013-03-06 23:48:18 +0900

    [Model/Log] Change: structure

--- Model/Log.model.php
+++ Model/Log.model.php
@@ -9,13 +9,46 @@ class Model_Log extends Model_Model
 {
 	private $log;
 	
-	function Set($log)
+	function Set( $log, $io=null )
 	{
-		$this->log[] = $log;
+		$stack = array();
+		$stack['io']  = $io;
+		$stack['log'] = $log;
+		$this->log[] = $stack;
 	}
 	
 	function Get()
 	{
-		return $this->log;
-	} 
+		if(!$this->Admin()){
+			return;
+		}
+		
+		$result = '<div class="table small">';
+		foreach( $this->log as $i => $stack ){
+			//  init
+			$class = null;
+			$io  = $stack['io'];
+			$log = $stack['log'];
+			
+			//  class
+			if(!is_null($io)){
+				$class = $io ? 'blue': 'red'; 
+			}
+			
+			//  table
+			$result .= '<div class="tr">';
+			$result .= '<div class="td">';
+			$result .= '<span class="'.$class.'">'.$log.'</span>';
+			$result .= '</div>';
+			$result .= '</div>';
+		}
+		$result .= '</div>';
+		
+		return $result;
+	}
+	
+	function Out()
+	{
+		print $this->Get();
+	}
 }
